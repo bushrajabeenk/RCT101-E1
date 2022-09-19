f2b4681bcb7bfe289f116c8a5b2598e9c696177e
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-09-19 15:30:41 +0530

    fc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import GroceryDetails from './components/Grocerydetails';
-
+import logo from "./logo.svg";
+import "./App.css";
+import GroceryDetails from "./components/Grocerydetails";
 
 function App() {
   return (
     <div className="App">
-     <GroceryDetails />
+      <GroceryDetails />
     </div>
   );
 }
--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -1,48 +1,43 @@
 import { useState } from "react";
-import './CartButton.css';
-
+import "./CartButton.css";
 
 // keep the add to cart component here
 const CartButton = () => {
-
-    //manage state of the count 
+  //manage state of the count
   const [count, setCount] = useState(0);
   const [status, setStatus] = useState(false);
 
   const handleInc = () => {
     setCount(count + 1);
-  }
+  };
 
   const handleDec = () => {
-    if(count > 0){
+    if (count > 0) {
       setCount(count - 1);
       setStatus(true);
     }
+    // render component 1
     setStatus(false);
     return;
-  }
+  };
 
   const handleChange = () => {
     setStatus(true);
+  };
+
+  // status is false, render this component - 1
+  if (!status) {
+    return <button onClick={handleChange}>Add to Cart</button>;
   }
 
-  
-
-  
-
-
-  if(!status)
-  {
-    return <button onClick={handleChange} >Add to Cart</button>
-  } 
-  
+  // status is true then render this component - 2
   return (
     <>
       {/* count with - and  + button */}
-      <div className="pmbtn" >
-          <button onClick={handleDec} >-</button>
-          <div className="count-item">{count}</div>
-          <button onClick={handleInc} >+</button>
+      <div className="pmbtn">
+        <button onClick={handleDec}>-</button>
+        <div className="count-item">{count}</div>
+        <button onClick={handleInc}>+</button>
       </div>
     </>
   );
